1f2a2fcad227d990643c386aca87667e3ae24354
alexdietz1988 <alex.dietz@gmail.com>
2022-09-12 10:11:04 -0400

    Refactor api calls

--- src/apis/backend.js
+++ src/apis/backend.js
@@ -1,50 +1,33 @@
 import axios from 'axios'
 
-// const backend = 'http://localhost:4000/'
-const backend = 'https://dietplanner-backend.herokuapp.com/'
+const diet = axios.create({
+    baseURL: 'https://dietplanner-backend.herokuapp.com/'
+})
 
 export function requestLogin(page, formData) {
-    return axios.post(backend + `auth/${page}`, {
-        username: formData.username,
-        password: formData.password
-    })
+    return diet.post(`auth/${page}`, formData)
 }
 
-export function requestAddMeal(user, formData) {
-    return axios.post(backend + `meal/?username=${user}`, {
-        username: user,
-        name: formData.name,
-        calories: formData.calories,
-        protein: formData.protein
-})
+export function requestAddMeal(username, formData) {
+    return diet.post(`meal/?username=${username}`, {username, ...formData})
 }
 
-export function requestEditMeal(user, mealId, formData) {
-    return axios.put(backend + `meal/?id=${mealId}`, {
-        username: user,
-        name: formData.name,
-        calories: formData.calories,
-        protein: formData.protein
-    })
+export function requestEditMeal(username, mealId, formData) {
+    return diet.put(`meal/?id=${mealId}`, {username, ...formData})
 }
 
 export function requestGetMeals(user) {
-    return axios.get(backend + `meal/?username=${user}`)
+    return diet.get(`meal/?username=${user}`)
 }
 
 export function requestDeleteMeal(mealId) {
-    return axios.delete(backend + `meal/?id=${mealId}`)
+    return diet.delete(`meal/?id=${mealId}`)
 }
 
 export function requestGetBasics(user) {
-    return axios.get(backend + `basics/?username=${user}`)
+    return diet.get(`basics/?username=${user}`)
 }
 
-export function requestEditBasics(user, formData) {
-    return axios.put(backend + `basics/?username=${user}`, {
-        username: user,
-        weight: formData.weight,
-        goal: formData.goal,
-        TDEE: formData.TDEE
-    })
+export function requestEditBasics(username, formData) {
+    return diet.put(`basics/?username=${username}`, { username, ...formData })
 }
