2494c034be4244c0f5257fb841b8a8ba8b64899f
alexdietz1988 <alex.dietz@gmail.com>
2022-09-16 16:35:09 -0400

    Hide password

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux'
 import Submit from '../Submit'
 
 function renderInput(props) {
+    console.log(props)
     return (
         <div className='field'>
             <label className='label' htmlFor={props.input.name}>{props.label}</label>
             <div className='control'>
-                <input className='input' {...props.input} required />
+                <input className='input' {...props.input} type={props.type} required />
             </div>
         </div>
     )
@@ -16,11 +17,11 @@ function renderInput(props) {
 let AuthForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit(props.onSubmit)}>
-            <Field name='user' component={renderInput} label='Username' />
+            <Field name='user' component={renderInput} label='Username' type='text' />
             {props.errorMessage === 'user already exists' ? 
                 <p className='help is-danger'>There's already an account with that username. If it's you, please login; otherwise, enter a different username.</p>
                 : ''}
-            <Field name='password' component={renderInput} label='Password' />
+            <Field name='password' label='Password' type='password' component={renderInput} />
             <Submit submitText='Submit' cancel='/' />
             {props.errorMessage === 'invalid username or password' ? <p>Invalid username or password.</p> : ''}
         </form>
