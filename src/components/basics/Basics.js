28e676de06cd47fa03fb6d4ba392ccc165a672ff
alexdietz1988 <alex.dietz@gmail.com>
2022-09-16 16:21:02 -0400

    Basics loads only when fetchCount changes

--- src/components/basics/Basics.js
+++ src/components/basics/Basics.js
@@ -1,14 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { fetchBasics } from '../../actions/basics'
 
 function Basics(props) {
+    const [loading, setLoading] = useState(true)
     useEffect(() => {props.fetchBasics()}, [])
+    const initialFetchCount = useRef(props.fetchCount)
 
-    return(
-        <section className='section'>
-            <h4 className='title is-4'>Basics</h4>
+    useEffect(() => {
+        if (props.fetchCount > initialFetchCount.current) {
+            setLoading(false)
+        }
+    }, [props.fetchCount])
+
+    function renderContent() {
+        return(
+            <>
             <div className='content'>
                 <p>You weigh <b>{props.basics.weight}</b> pounds.</p>
                 <p>You burn <b>{props.basics.TDEE}</b> calories per day.</p>
@@ -16,12 +24,23 @@ function Basics(props) {
             </div>
             
             <Link to='/basics/edit'><button className='button is-warning'>Update</button></Link>
+            </>
+        )
+    }
+
+    return(
+        <section className='section'>
+            <h4 className='title is-4'>Basics</h4>
+            {loading ? <div>Loading...</div> : renderContent()}
         </section>
     )
 }
 
 function mapStateToProps(state) {
-    return {basics: state.basics.data}
+    return {
+        basics: state.basics.data,
+        fetchCount: state.basics.fetchCount
+    }
 }
 
 export default connect(mapStateToProps, { fetchBasics })(Basics)
